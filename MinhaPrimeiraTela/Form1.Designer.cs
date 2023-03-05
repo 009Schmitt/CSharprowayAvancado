0e329a8af08793627058a14c7ca76ff564992263
009Schmitt <34982675+009Schmitt@users.noreply.github.com>
2023-03-05 18:57:14 -0300

    Add files via upload

---
+++ MinhaPrimeiraTela/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace MinhaPrimeiraTela
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLogar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(385, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Priston Tale";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(325, 120);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(273, 38);
+            this.txtLogin.TabIndex = 1;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(325, 214);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(273, 38);
+            this.txtSenha.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(422, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Login";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(422, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Senha";
+            // 
+            // btnLogar
+            // 
+            this.btnLogar.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnLogar.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F);
+            this.btnLogar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnLogar.Location = new System.Drawing.Point(152, 258);
+            this.btnLogar.Name = "btnLogar";
+            this.btnLogar.Size = new System.Drawing.Size(630, 227);
+            this.btnLogar.TabIndex = 5;
+            this.btnLogar.Text = "👌";
+            this.btnLogar.UseVisualStyleBackColor = false;
+            this.btnLogar.Click += new System.EventHandler(this.btnLogar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 497);
+            this.Controls.Add(this.btnLogar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLogar;
+    }
+}
+
---
+++ MinhaPrimeiraTela/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinhaPrimeiraTela
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogar_Click(object sender, EventArgs e)
+        {
+            string[] bancoDeDados = File.ReadAllLines(@"C:\BD\Cliente.txt");
+            //List<string> bancoDeDados = bD.ToList<string>();
+            //bancoDeDados.RemoveAt(0);
+
+
+            bool loginEncontrado = false;
+            foreach (var item in bancoDeDados)
+            {
+                string[] temp = item.Split('|');
+                try
+                {
+                    Cliente cli = new Cliente()
+                    {
+                        ID = Convert.ToInt32(temp[0]),
+                        Nome = temp[1],
+                        Idade = Convert.ToInt32(temp[2]),
+                        Cpf = temp[3],
+                        Login = temp[4],
+                        Senha = temp[5]
+                    };
+
+                    if (cli.Login == txtLogin.Text)
+                    {
+                        loginEncontrado = true;
+                        if (cli.Senha == txtSenha.Text)
+                        {
+                            MessageBox.Show("Login Efetuado");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Senha incorreta");
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+
+            if (!loginEncontrado)
+            {
+                MessageBox.Show("Login Não encontrado na base");
+            }
+        }
+
+    }
+}
